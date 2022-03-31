80f5df6515735cd149a3d3fababa17aa89de04a0
rupesh911 <81091408+rupesh911@users.noreply.github.com>
2022-03-31 22:09:48 +0530

    inn

--- Controller/user.js
+++ Controller/user.js
@@ -11,6 +11,7 @@ exports.signup = async (req,res,next)=>{
         console.log("Creating User");
     const user = await userModel.create(req.body)
     const token = user.getSignedJwtToken();
+    console.log(token)
     res.status(200).json({
         success:true,
         token:token
@@ -20,7 +21,7 @@ exports.signup = async (req,res,next)=>{
         console.log("User Already Exist");
         res.status(400).json({
             success:false,
-            err:"User Already Exist"
+            err:"User Already Est"
         })
     }
 
